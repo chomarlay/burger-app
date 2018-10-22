828f8ee80103d57b28777cb532be44249ecbcc16
chomarlay <chomarlay.aung@gmail.com>
2018-10-23 06:07:46 +1100

    added  build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,18 @@ import Auxilary from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+    state={ ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese:0,
+        meat: 0
+
+    }}
     render () {
         return (
         <Auxilary>
             <main>
-                <Burger/>
+                <Burger ingredients ={ this.state.ingredients}/>
                 <p>Burger Controls</p>
             </main>
         </Auxilary>
