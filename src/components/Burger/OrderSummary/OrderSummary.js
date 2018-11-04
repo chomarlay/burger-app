a4dfbaf29dc564ed4f9a11d6fbc12cefd8119bf5
chomarlay <chomarlay.aung@gmail.com>
2018-11-05 06:30:09 +1100

    added order total to order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -15,6 +15,7 @@ const orderSummary = (props) => {
     return(
     <Auxilary>
         <h3>Order Summary</h3>
+        <p>Total Price : <strong>{props.totalPrice.toFixed(2)}</strong></p>
         <p>A burger with the following ingredients</p>
         <ul>
             {ingredientSummary}
