e786356d544898746a5fc58b122514b6f4d9392e
chomarlay <chomarlay.aung@gmail.com>
2018-11-15 06:13:52 +1100

    restructure and perf improvements

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Auxilary from '../../../hoc/Auxilary';
+import Auxilary from '../../../hoc/Auxilary/Auxilary';
 import Button from '../../UI/Button/Button';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
