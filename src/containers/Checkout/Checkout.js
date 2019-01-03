489c18e8df1f1778165d8301b17a9df26179e3fe
chomarlay <chomarlay.aung@gmail.com>
2019-01-04 00:11:36 +1100

    routing added

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,10 +5,30 @@ class Checkout extends Component {
     state={ 
         ingredients: {salad:1, cheese:1, bacon:1}
     }
+    componentDidMount(){
+        const query = new URLSearchParams(this.props.location.search);
+        const theIngredients = {};
+        for (let param of query.entries()) {
+            theIngredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients:theIngredients});
+    }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
     render() {
         return (
         <div>
-            <CheckoutSummary ingredients={this.state.ingredients} />
+            <CheckoutSummary 
+                ingredients={this.state.ingredients}
+                checkoutCancelled={this.checkoutCancelledHandler}
+                checkoutContinued={this.checkoutContinuedHandler}
+            
+            />
         </div>
         )
     }
