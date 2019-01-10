fc0471f382551d1027a1d44ed39338e592c25561
chomarlay <chomarlay.aung@gmail.com>
2019-01-11 05:44:07 +1100

    checkout summary styling

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -8,7 +8,7 @@ const CheckoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>Enjoy your burger </h1>
-            <div style={{width: "100%", margin:"auto", textAlign:"center"}}>
+            <div style={{width: '100%', margin:'auto'}}>
                 <Burger ingredients={props.ingredients} />
             </div>
             <Button btnType="Danger" clicked={props.checkoutCancelled}>CANCEL</Button>
