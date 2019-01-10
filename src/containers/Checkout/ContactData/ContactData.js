b8a45f1a2bc34552a60d47778a27e0a48fa40437
chomarlay <chomarlay.aung@gmail.com>
2019-01-11 05:40:33 +1100

    forms and validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component{
 
@@ -43,10 +44,10 @@ class ContactData extends Component{
     render(){ 
         let form=(
             <form>
-            <input className={classes.Input} type='text' name="name" placeholder="Your Name"></input>
-            <input className={classes.Input} type='email' name="name" placeholder="Your Email"></input>
-            <input className={classes.input} type='text' name="street" placeholder="Street"></input>
-            <input className={classes.Input} type='text' name="postalCode" placeholder="Postal Code"></input>
+            <Input inputtype='input' type='text' name="name" placeholder="Your Name"></Input>
+            <Input inputtype='input' type='email' name="name" placeholder="Your Email"></Input>
+            <Input inputtype='input' type='text' name="street" placeholder="Street"></Input>
+            <Input inputtype='input' type='text' name="postalCode" placeholder="Postal Code"></Input>
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
         );
